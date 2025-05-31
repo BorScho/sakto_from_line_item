7a95f93a0903aaa5c6acd9fa1597e0061794ec70
BorScho <51828456+BorScho@users.noreply.github.com>
2025-05-31 16:04:09 +0200

    initial commit

---
+++ notebooks/utils.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.model_selection import train_test_split
+import re
+
+
+def plot_confusion_matrix(y_test, y_pred, x_labels, y_labels, cmap="Blues"):
+    xticklabels = x_labels
+    yticklabels = y_labels
+    # define confusion matrix:
+    conf_matrix = np.zeros((len(np.unique(y_test)), len(np.unique(y_test))), dtype=int)
+    # count how often each true class /pred class combination occurs:
+    for i in range(len(y_test)):
+        true_class = y_test[i]
+        pred_class = y_pred[i]
+        conf_matrix[true_class, pred_class] += 1
+    # vizualize the matrix:
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, fmt="d", cmap=cmap, xticklabels=xticklabels, yticklabels=yticklabels)
+    #sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=np.unique(y_train), yticklabels=np.unique(y_train))
+    plt.xlabel("Predicted Class")
+    plt.ylabel("True Class")
+    plt.title(f"Confusion Matrix - Support: {len(y_test)}")
+    plt.show()
