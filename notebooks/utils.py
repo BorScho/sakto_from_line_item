9b0b16f54254ba9c65871d419eada2a3d99ec389
BorScho <51828456+BorScho@users.noreply.github.com>
2025-06-10 19:43:40 +0200

    corrected plot_confusion_matrix() with internal encoding of y_test and y_pred

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,11 +3,13 @@ import seaborn as sns
 import numpy as np
 from sklearn.model_selection import train_test_split
 import re
+from sklearn.preprocessing import LabelEncoder
 
-
-def plot_confusion_matrix(y_test, y_pred, x_labels, y_labels, cmap="Blues"):
+ #OLD variant:
+""" def plot_confusion_matrix(y_test, y_pred, x_labels, y_labels, cmap="Blues"):
     xticklabels = x_labels
     yticklabels = y_labels
+    
     # define confusion matrix:
     conf_matrix = np.zeros((len(np.unique(y_test)), len(np.unique(y_test))), dtype=int)
     # count how often each true class /pred class combination occurs:
@@ -22,4 +24,36 @@ def plot_confusion_matrix(y_test, y_pred, x_labels, y_labels, cmap="Blues"):
     plt.xlabel("Predicted Class")
     plt.ylabel("True Class")
     plt.title(f"Confusion Matrix - Support: {len(y_test)}")
+    plt.show() """
+
+
+def plot_confusion_matrix(y_test, y_pred, x_labels, y_labels, header_text="", cmap="Blues"):
+    xticklabels = x_labels
+    yticklabels = y_labels
+
+    complete = np.unique(np.concatenate((y_test,y_pred), axis=0))
+    le = LabelEncoder()
+    le.fit(complete)
+    y_test_indices = le.transform(y_test)
+    y_pred_indices = le.transform(y_pred)
+    
+    # define confusion matrix:
+    conf_matrix = np.zeros((len(complete), len(complete)), dtype=int)
+    # count how often each true class /pred class combination occurs:
+    for i in range(len(y_test)):
+        true_class = y_test_indices[i]
+        pred_class = y_pred_indices[i]
+        conf_matrix[true_class, pred_class] += 1
+    # vizualize the matrix:
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, fmt="d", cmap=cmap, xticklabels=xticklabels, yticklabels=yticklabels)
+    #sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=np.unique(y_train), yticklabels=np.unique(y_train))
+    plt.xlabel("Predicted Class")
+    plt.ylabel("True Class")
+    
+    if header_text=="":
+        plt.title(f"Confusion Matrix - Support: {len(y_test)}")
+    else:
+        plt.title(f"{header_text} - Support: {len(y_test)}")
+
     plt.show()
